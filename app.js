56aa574c62ae79582512dab63cf6c24dc199c1b5
Riley Hess <RileyH@beckhoff.com>
2024-10-28 13:06:22 -0500

    adding in changes for functionality of a single game play

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let num = Math.floor(Math.random() * 3) + 1;
 }
 
 
-console.log(computer());
+//console.log("Computers choice is: ", computer());
 
 /* Function to get the players choice */
 
@@ -30,8 +30,56 @@ function player(){
 }
 
 
-console.log(player());
-
 
 let computerScore = 0;
 let humanScore = 0;
+
+
+/*Adding in logic for the game play*/
+
+function playRound(humanChoice, computerChoice){
+
+    console.log(humanChoice);
+    console.log(computerChoice);
+humanChoice.toLowerCase();
+
+
+if (humanChoice === computerChoice || computerChoice === humanChoice){
+    console.log("you tied");
+    return;
+}
+/*human win cases*/
+else if (humanChoice === "rock" && computerChoice === "scissors"){
+    console.log("Human has won");
+    return humanScore + 1;
+}
+else if (humanChoice === "paper" && computerChoice === "rock"){
+    console.log("Hman has won");
+    return humanScore +1;
+}
+else if (humanChoice ==="scissors" && computerChoice === "paper"){
+    console.log("Hman has won");
+    return humanScore +1;
+}
+
+/*computer win cases*/
+else if (computerChoice === "rock" && humanChoice === "scissors"){
+    console.log("Computer has won, human race is doomed");
+    return computerScore +1;
+}
+else if (computerChoice === "paper" && humanChoice === "rock"){
+    console.log("Computer has won, human race is doomed");
+    return computerScore +1;
+}
+else if (computerChoice ==="scissors" && humanChoice === "paper"){
+    console.log("Computer has won, human race is doomed");
+    return computerScore +1;
+}
+
+
+
+
+}
+
+playRound(computer(), player());
+console.log('the score is Computer: ${computerScore}  Human: ${humanScore}');
