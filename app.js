dbeffd64572247782102367e3aa9da7772f1ad19
Riley Hess <RileyH@beckhoff.com>
2024-10-31 13:21:02 -0500

    adding in some touch ups to get the javascript code working better, as well as some comment housekeeping to make the code more readable

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 /*Function that will randomly generate a number between 1 and 3, this will be used to pick from 'rock', 'paper' and 'scissors'
 in a switch statement*/
 
+
+
+/* ------- global variabels declared here  ------- */
+let enterButton = document.querySelector('.Enter');
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+
+
+
+const container = document.querySelector('.container');
+
+
+let rockButton = document.createElement('button');
+rockButton.classList.add('rockButton');
+rockButton.textContent = "Rock";
+let paperButton = document.createElement('button');
+paperButton.classList.add('paperButton');
+paperButton.textContent = "Paper";
+let scissorsButton = document.createElement('button');
+scissorsButton.classList.add('scissorsButton');
+scissorsButton.textContent = "Scissors";
+
+
+
+
+/* -------  Computer function to randomly generate a number 1-3 and return 'rock', 'paper', or 'scissors'  ------- */
+
 function computer(){
+
+
+
 
 let num = Math.floor(Math.random() * 3) + 1;
 
@@ -18,99 +52,137 @@ let num = Math.floor(Math.random() * 3) + 1;
 }
 
 
-//console.log("Computers choice is: ", computer());
+/* ------- Funciton for the logic to play a round of rock paper scissors, which takes the return value of computer() and player selection as inputs  ------- */
 
-/* Function to get the players choice */
-
-function player(){
-
-    let choice = prompt("Enter in your choice/n Rock.. Paper.. or Scissors..: ");
-
-    return choice;
-}
-
-
-
-let computerScore = 0;
-let humanScore = 0;
-
-
-/*Adding in logic for the game play*/
 
 function playRound(humanChoice, computerChoice){
 
-    console.log(humanChoice);
-    console.log(computerChoice);
     humanChoice.toLowerCase();
-    
 
-
-if (humanChoice === computerChoice || computerChoice === humanChoice){
+if (humanScore === 5){
+    console.log('human score is 5');
+    humanScore = 0;
+    computerScore = 0;
+    alert("Humans Won!");
+    Enter(5);
+}
+else if (computerScore === 5){
+    console.log('computer score is 5');
+    humanScore = 0;
+    computerScore = 0;
+    alert("Computers Won!");
+    Enter(5);
+}
+else if (humanChoice === computerChoice || computerChoice === humanChoice){
     console.log("you tied");
-    return;
+    
 }
 /*human win cases*/
 else if (humanChoice === "rock" && computerChoice === "scissors"){
     console.log("Human has won");
-    return humanScore++;
+    humanScore++;
+    
 }
 else if (humanChoice === "paper" && computerChoice === "rock"){
     console.log("Hman has won");
-    return humanScore++;
+    humanScore++;
+    
 }
 else if (humanChoice ==="scissors" && computerChoice === "paper"){
     console.log("Hman has won");
-    return humanScore++;
+    humanScore++;
+    
 }
 
 /*computer win cases*/
 else if (computerChoice === "rock" && humanChoice === "scissors"){
     console.log("Computer has won, human race is doomed");
-    return computerScore++;
-}
+    computerScore++;
+    
+} 
 else if (computerChoice === "paper" && humanChoice === "rock"){
     console.log("Computer has won, human race is doomed");
-    return computerScore++;
+    computerScore++;
+    
 }
 else if (computerChoice ==="scissors" && humanChoice === "paper"){
     console.log("Computer has won, human race is doomed");
-    return computerScore++;
+    computerScore++;
+    
 }
 
 
-
-
 }
 
 
+/* ------- function to allow the player to enter the game and hwen the game is finished to reset the game ------- */
 
-/* adding in game engine */
+function Enter(fin){
 
-function playGame(){
-    playRound(computer(), player());
-    console.log('the score is Computer: ',computerScore, 'Human:', humanScore);
+    let round = 0;
+    round = fin;
 
-    playRound(computer(), player());
-    console.log('the score is Computer: ',computerScore, 'Human:', humanScore);
+   switch( round) {
+    //inital round ask player to play the game
 
-    playRound(computer(), player());
-    console.log('the score is Computer: ',computerScore, 'Human:', humanScore);
+    case 0 : addGamePieces();
+                break;
+    case 5 : removeGamePieces();
+            break;
 
-    playRound(computer(), player());
-    console.log('the score is Computer: ',computerScore, 'Human:', humanScore);
-
-    playRound(computer(), player());
-    console.log('the score is Computer: ',computerScore, 'Human:', humanScore);
+            
 
 
 
-    if(humanScore > computerScore){
-        console.log("Humans are victorious");
-    }
-    else {
-        console.log("Computer rule, humans are doomes");
-    }
+   }
+} 
+/* ------- functions to add the game pieces: 'rock', 'paper' and 'scissor' buttons and add in the enter button to the dom   ------- */
+
+function addGamePieces(){
+    container.appendChild(rockButton);
+    container.appendChild(paperButton);
+    container.appendChild(scissorsButton);
+
+    container.removeChild(enterButton);
+
+    rockButton.addEventListener('click', () => {
+        playRound('rock', computer());
+    });
+    
+    paperButton.addEventListener('click', () => {
+        playRound('paper', computer());
+    });
+    
+    scissorsButton.addEventListener('click', () => {
+        playRound('scissors', computer());
+    });
 
 }
 
-playGame();
+/* ------- functions to remove the game pieces: 'rock', 'paper' and 'scissor' buttons and re add int he enter button to the dom   ------- */
+
+function removeGamePieces(){
+    container.removeChild(rockButton);
+    container.removeChild(paperButton);
+    container.removeChild(scissorsButton);
+    
+    container.appendChild(enterButton);
+
+    rockButton.addEventListener('click', () => {
+        playRound('rock', computer());
+    });
+    
+    paperButton.addEventListener('click', () => {
+        playRound('paper', computer());
+    });
+    
+    scissorsButton.addEventListener('click', () => {
+        playRound('scissors', computer());
+    });
+
+}
+
+/* ------- Adding event listener to the enter button which calls the enter function on 'click' ------- */
+enterButton.addEventListener('click', ()=>{
+    Enter(0);
+})
